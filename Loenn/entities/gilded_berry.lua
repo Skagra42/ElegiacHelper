a2d08cd5a64a6f9ebf25dc010e128102d04577b7
Skagra42 <herobrineclone42@gmail.com>
2026-01-01 19:15:15 -0700

    Gilded berry lua file.

---
+++ Loenn/entities/gilded_berry.lua
@@ -0,0 +1,9 @@
+local gildedBerry = {}
+gildedBerry.name = "ElegiacHelper/GildedBerry"
+gildedBerry.depth = -100
+gildedBerry.texture = "collectables/goldberry/idle00"
+gildedBerry.justification = {0.5, 1.0}
+gildedBerry.placements = {
+    name = "gilded_berry"
+}
+return gildedBerry
